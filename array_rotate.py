f0b852ae0bdf9e68b5d095a728aec30dc76d2c23
Sourav kumar <33771969+souravs17031999@users.noreply.github.com>
2019-10-19 22:38:22 +0530

    Update array_rotate.py

--- array_rotate.py
+++ array_rotate.py
@@ -1,73 +1,21 @@
-# program for rotating array by k times using two methods
+# Program for finding all unqiue pairs for which sums up to some target value in a array
 
-# First method using reversal algorithm
-def reverse(l, start, end):
-    '''
-    parameters :
-    l - list of elements
-    start - start index (leftmost)
-    end - end index (rightmost)
-
-    '''
+def array_target_sum_main(l, target):
+    l.sort()
+    start = 0
+    end = len(l) - 1
     while(start < end):
-        l[start], l[end] = l[end], l[start]  # swapping start index element with last  element index till start coincides with end
-        start += 1
-        end -= 1
+        if(l[start] + l[end] == target):
+            print(f"{l[start]} and {l[end]}")
+        if(l[start] + l[end] > target):
+            end -= 1
+        else:
+            start += 1
 
-def reverse_rotate(l, k):
-    '''
-    parameters :
-    l - list of elements
-    k - number by which array is rotated
 
-    '''
-    n = len(l)
-    reverse(l, 0, k-1)  # reverse first half of array
-    reverse(l, k, n-1)  # reverse last half of array
-    reverse(l, 0, n-1)  # reverse overall array
-    print(l)
-
-# second method using temporary array
-def left_rotate_temp(l, k):
-    '''
-    parameters :
-    l - list of elements
-    k - number by which array is rotated
-
-    '''
-    temp = []
-    n = len(l)
-    # first we shift all k elements in a temporary array
-    for i in range(0, k):
-        temp.append(l[i])
-    j = 0
-    # then shift the remaning element one by one
-    for i in range(k, n):
-        l[j] = l[i]
-        j += 1
-    j = 0
-    # shift back all the elements from temporary to main array
-    for i in range(n - k, n):
-        l[i] = temp[j]
-        j += 1
-    print(l)
-
-def main_rotate(l, k):
-    '''
-    parameters :
-    l - list of elements
-    k - number by which array is rotated
-
-    '''
-    l1 = l.copy()
-    print(f"Original list: {l}")
-    print("Modified list after rotation using temp array :")
-    left_rotate_temp(l, k)
-    print(f"Modified list after using reverse rotate : ")
-    reverse_rotate(l1, k)
-
-if __name__ == "__main__":
-    print("Give the elements of the original list followed by number of rotations : ")
-    u_input = list(map(int, input().strip().split()))
-    r_input = int(input().strip())
-    main_rotate(u_input, r_input)
+n = int(input())
+l = []
+for i in range(n):
+    l.append(int(input()))
+target = int(input())
+array_target_sum_main(l, target)
