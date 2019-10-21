88d23524e589a3d11fa28e6fb1d60c1ab7b4fb91
sourav kumar <souravs_1999@rediffmail.com>
2019-10-21 19:55:33 +0530

    update whole program optimizing it

--- array_target_sum.py
+++ array_target_sum.py
@@ -1,30 +1,16 @@
 # Program for finding all unqiue pairs for which sums up to some target value in a array
-def binary_search(l, start, end, key):
-    while(start <= end):
-        middle = start + (end - start) // 2
-        if key < l[middle]:
-            end = middle - 1
-        elif key > l[middle]:
-            start = middle + 1
-        else:
-            return 1
-    return 0
 
 def array_target_sum_main(l, target):
+    start = 0
+    end = len(l) - 1
     l.sort()
-    n = len(l)
-    a = []
-    for i in range(len(l)):
-        if l[i] > 0:
-            result = target - l[i]
+    while(start < end):
+        if(l[start] + l[end] == target):
+            print(f"{l[start]} {l[end]}")
+        if(l[start] + l[end] > target):
+            end -= 1
         else:
-            result = l[i] - target
-        if l[i] != result:
-            if(binary_search(l, 0, n, result)):
-                if l[i] not in a or result not in a:
-                    print("{} and {}".format(l[i], result))
-                    a.append(l[i])
-                    a.append(result)
+            start += 1
 
 
 n = int(input())
