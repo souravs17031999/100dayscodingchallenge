6ab61c3abc239c55c5a25e3fa75d3f3d12c77e8f
Sourav kumar <souravs_1999@rediffmail.com>
2020-01-29 08:40:28 +0530

    updating complexity info for if in python operator

--- check_string_rotations.py
+++ check_string_rotations.py
@@ -3,7 +3,8 @@
 # IDEA: first logic comes to mind is to check if for every possible one unit shifts , if we can reach the other string, then we can say they are rotations.
 # Next approach would be that we can make a copy of string and concatenate it with first string, then check if second string occurs in it, if it has to be rotated, then it must be occuring in some part (substring) of the first string.
 # Both of the above approach will take 0(N*N), where N lengths of two strings and 0(N) space.
-
+# EDIT : Python if in operator for string matching uses mixture of boyer moore and horsepool algorithms which takes 0(N) worst case, 
+# Now, we can achieve 0(N) complexity without any doubt in below algo but space is again 0(N).
 # See the code for naive modified solution below
 def rotate_check(str1, str2):
     str_length2 = len(str1)
