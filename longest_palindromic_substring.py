31ff55c7d4b2d6ae1a1fe6ff2456704318b272ac
Sourav kumar <souravs_1999@rediffmail.com>
2020-03-16 22:20:02 +0530

    Adding some additional comments

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -11,9 +11,12 @@ def length_palindrome(s, start, end):
     if s == None or start > end:
         return 0
     n = len(s)
+    # keep moving start pointer left and end pointer right , 
+    # until all matches are made 
     while start >= 0 and end < n and s[start] == s[end]:
         start -= 1
         end += 1
+    # returning the length of current palindromic substring found
     return end - start - 1
 
 # function for generating longest substrings of all possible substrings
@@ -25,10 +28,17 @@ def longest_palindrome(s):
     cur_max = 0
     start, end = 0, 0
     for i in range(0, n):
+        # for handling odd length palindromes 
         l1 = length_palindrome(s, i, i)
+        # fkr handling even length palindromes
         l2 = length_palindrome(s, i, i + 1)
+        # anyone one of them could be longer so making track of that
         cur_max = max(l1, l2)
+        # keeping track of overall length maximum found so far
         max_length = max(max_length, cur_max)
+        # we can stop here if we are just required to find length
+        # but this is needed as we need to print the substring
+        # so for that we need both start and end pointers (indices of chars)
         if cur_max > end - start:
             start = i - ((cur_max - 1) // 2)
             end = i + (cur_max // 2)
