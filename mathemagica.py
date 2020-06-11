559fdf1049c991605a81dfdcd0a1162785661689
sourav kumar <souravs_1999@rediffmail.com>
2020-06-11 16:29:24 +0530

    update function for fast exponentiation algorithm

--- mathemagica.py
+++ mathemagica.py
@@ -36,3 +36,15 @@ class mathemagica:
 
     def count_digits(n):
         return int(math.log10(n) + 1)
+
+    # fast exponentiation algo
+    def expo(a, n):
+        ans = 1
+        while n > 0:
+            if n & 1:
+                ans *= a
+
+            a *= a
+            n = n >> 1
+
+        return ans
