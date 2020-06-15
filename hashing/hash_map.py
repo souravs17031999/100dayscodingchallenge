53486f13042cc216aa35c6b3d7b65ff046f6dff2
sourav kumar <souravs_1999@rediffmail.com>
2020-06-15 19:30:31 +0530

    Updated some logic about hash function
    New ideas about hash functions
    but improvement needed by replacing the current 2-d struture into linked lists type.

--- hashing/hash_map.py
+++ hashing/hash_map.py
@@ -1,3 +1,6 @@
+# Program to create hash table from scratch
+# We can create hash functions in different ways
+
 class hashmap:
     def __init__(self, size):
         self.size = size
@@ -12,12 +15,10 @@ class hashmap:
     def add(self, key, value):
         index = self.get_hash(key)
         if self.map[index] == None:
-            self.map[index] = [key, value]
+            self.map[index] = list([key, value])
         else:
             for pair in self.map[index]:
-                if pair != None:
-                    if pair[0] == key:
-                        return pair    
+                    print(pair)
             self.map[index].append([key, value])
 
     def get_value(self, key):
@@ -45,5 +46,17 @@ if __name__ == '__main__':
     h.add('sourav kumar', 100)
     h.add('elon musk', 95)
     h.add('jennifer lawrence', 98)
-    h.add('sourav kumar', 120)
+    #h.add('sourav kumar', 120)
     h.print_hashmap()
+
+
+# some of the important hash functions are shown below
+
+# def getHash(self, key):
+#     index, p = 0, 1
+#     for i in range(len(key)):
+#         index = index + (ord(key[i]) * p) % self.size
+#         index = index % self.size
+#         p = (p * 37) % self.size
+#
+#     return index
