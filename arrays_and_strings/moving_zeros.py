2f05508f707a6cde487abb3a77d758f3d14a3fa7
Sourav kumar <souravs_1999@rediffmail.com>
2020-07-06 18:39:13 +0530

    adding some useful comments, and time and space complexity info

--- arrays_and_strings/moving_zeros.py
+++ arrays_and_strings/moving_zeros.py
@@ -1,5 +1,9 @@
 # program for moving all zeroes to the end and all
 # non zero elements at the front with the same relative ordering.
+# Optimized approach : 
+# Idea here is to take a counter, and then simply for every non-zero element, we can increment counter and place the element at that index of the array, in this way, all non-zero
+# elements will be at the front maintaining their relative order, then simply put zeroes for the remaining lenght of the array.
+# TIME : 0(N), SPACE : 0(1).
 
 def moveZeroes(nums) -> None:
         n = len(nums)
