c4adb001064a475bd267479739e1497784f86867
sourav kumar <souravs_1999@rediffmail.com>
2020-07-07 23:17:15 +0530

    Update comments on time and space complexity

--- arrays_and_strings/segregate_zeroes_ones_efficient.py
+++ arrays_and_strings/segregate_zeroes_ones_efficient.py
@@ -3,7 +3,9 @@
 # right pointers (decrementing) until we hit 0, and then, we will be able to actually stop at that position and now,
 # if left < right, then it means that "1" is at left pointer, "0" is at right pointer, so we need to swap these both.
 # In this way, all 0's will be at front, and then all 1's will be at back of the array.
-# TIME : 0(N), SPACE : 0(1) 
+# This is also efficient in a way such that only one pass is required otherwise we can also simply count the number of times "1" and "0" occurs
+# and simply copy to the original array resulting in segregatation but that can take more than one pass.
+# TIME : 0(N), SPACE : 0(1)
 
 import random
 def segregate(arr, n):
