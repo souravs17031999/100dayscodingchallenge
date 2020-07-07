e64ab0b9dd201291f285dab48f85417dff4c1be4
sourav kumar <souravs_1999@rediffmail.com>
2020-07-07 21:01:33 +0530

    Selection sort in python3 program

---
+++ sorting algo/selection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr, n):
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
+
+if __name__ == '__main__':
+    arr = [-90, 121, 0, -60, 1000]
+    selection_sort(arr, 5)
+    print(arr)
