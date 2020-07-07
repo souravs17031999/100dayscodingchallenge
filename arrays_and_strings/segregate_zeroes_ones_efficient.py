c7194d92eccaf3dba5f253ec70c6b0bd191216b4
sourav kumar <souravs_1999@rediffmail.com>
2020-07-07 21:56:00 +0530

    Create program to segregate 1's and 0's efficiently

---
+++ arrays_and_strings/segregate_zeroes_ones_efficient.py
@@ -0,0 +1,30 @@
+# Program for zeros and ones segregatation, efficiently.
+# Idea is to basically keep two pointers and then, we can keep on moving left pointers (incrementing) until we hit 1, and then keep on moving
+# right pointers (decrementing) until we hit 0, and then, we will be able to actually stop at that position and now,
+# if left < right, then it means that "1" is at left pointer, "0" is at right pointer, so we need to swap these both.
+# In this way, all 0's will be at front, and then all 1's will be at back of the array.
+# TIME : 0(N), SPACE : 0(1) 
+
+import random
+def segregate(arr, n):
+    left, right = 0, n - 1
+    while left < right:
+
+        while arr[left] == 0 and left < right:
+            left += 1
+
+        while arr[right] == 1 and left < right:
+            right -= 1
+
+        if left < right:
+            arr[left], arr[right] = 0, 1
+            left += 1
+            right -= 1
+
+    return arr
+
+if __name__ == '__main__':
+    arr = [random.randint(0, 1) for _ in range(10)]
+    print(arr)
+    segregate(arr, len(arr))
+    print(arr)
