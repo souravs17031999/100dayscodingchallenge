d9f258890844083e0631ad1230905979ca8d7ba4
sourav kumar <souravs_1999@rediffmail.com>
2020-07-22 21:36:05 +0530

    Counting unique BST's with n nodes

---
+++ binary search trees/counting_unique_BST_n_nodes.py
@@ -0,0 +1,7 @@
+# Program for counting unique BST's with 'n' number of nodes.
+# From counting and observing as done for binary unlabelled trees, we can observe this follows catalan numbers,
+# and we can directly return the value of catalan numbers.
+# C : 1, 1, 2, 5, 14.....
+
+# code will be similar to binary tree unlabelled counting.
+# Also approach can be DP one or the binom formula.
