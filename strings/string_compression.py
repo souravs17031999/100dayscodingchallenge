e08fbc593d9b1fdee5292980e08e3a64200ca0c5
sourav kumar <souravs_1999@rediffmail.com>
2020-08-11 10:10:33 +0530

    Update few lines for better optimization

--- strings/string_compression.py
+++ strings/string_compression.py
@@ -2,16 +2,19 @@
 # we need to take only the count of the repeated char and a count of just that char.
 
 def compress_string(s):
-    temp, count = s[0], 1
-    print(temp, end = "")
+    result, count = "", 1
+    prev = s[0]
     for i in range(1, len(s)):
-        if s[i] == s[i - 1]:
+        if s[i] == prev:
             count += 1
         else:
-            temp = s[i]
-            print(f"{count}{temp}", end = "")
+            result = f"{result}{str(count)}{prev}"
+            #result += prev
+            prev = s[i]
             count = 1
-    print(count)
+
+    result = f"{result}{str(count)}{prev}"
+    print(result)
 
 if __name__ == '__main__':
     s = "aaabbccds"
