70982c60e346bb038805ba0e96ef40a5e42aa113
sourav kumar <souravs_1999@rediffmail.com>
2020-08-11 23:22:23 +0530

    delelte the wrong method

---
+++ strings/longest_substring_k_unique_chars.py
@@ -0,0 +1,8 @@
+# Program to print the longest substring with exactly k unique characters , if more than one
+# then print any one of them.
+# "aabbcc", k = 1
+# So, we can see what all ssubstrings we can generate :
+# a, aa, aab, aabb, .....
+# but the catch is to have a counter var for max len obtained so far and also if at anytime
+# we found a substring with exactly k unique chars, so we can store it in a ans var.
+# aa, 
