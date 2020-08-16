917fa20752d8543f84d78fd2c337bd7b8c9a87ab
sourav kumar <souravs_1999@rediffmail.com>
2020-08-16 12:48:55 +0530

    adding one new hashing startegy

--- strings/string_hashing_template.py
+++ strings/string_hashing_template.py
@@ -1,5 +1,9 @@
+# There can really huge number of cases where string hashing is really powerful.
+
+# polynomial rolling hash function
+# hash(s) = sigma(s[i] * p^i) % m
 def get_hash(s):
-    p = 31
+    p = 31  # if we are using both ascii lower and upper case letters, then use  p = 53.
     m = 10 ** 9 + 9
     hash_value = 0
     power = 1
@@ -8,6 +12,13 @@ def get_hash(s):
         power = (power * p) % m
 
     return hash_value
+
+# this is a simple hash function
+def get_hash(key):
+    sum = 0
+    for i in key:
+        sum += ord(i)
+    return sum % 10
 
 if __name__ == '__main__':
     print(get_hash("souravkumar"))
