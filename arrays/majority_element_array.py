ed9f257d53405398303a45bd5da691ebeec691ef
sourav kumar <souravs_1999@rediffmail.com>
2020-08-21 08:53:46 +0530

    Compute majority element - N//3

--- arrays/majority_element_array.py
+++ arrays/majority_element_array.py
@@ -1,22 +1,69 @@
 # Program to find the majority element that is the occurence should be greater than N/2 , where N is the size of the array
-
-# METHOD 4 STARTS
+# BELOW PROGRAMS IN VARIOUS SECTIONS ARE FOR N/2, N/3.... MAJORITY ELEMENTS.
+# ----------------------------------------------------------------------------------------------------------------
+# NOTE :
+# There can be at most one majority element which is more than ⌊n/2⌋ times.
+# There can be at most two majority elements which are more than ⌊n/3⌋ times.
+# There can be at most three majority elements which are more than ⌊n/4⌋ times.
+# -----------------------------------------------------------------------------------------------------------------
+# MOST OPTIMIZED METHOD :
 # THIS USES BOYER - MOORE VOTING ALGORITHM
 # BEST METHOD , TAKES 0(N) TIME, 0(1) SPACE BUT AGAIN IT ASSUMES MAJORITY ELEMENT EXISTS
 # logic is to simply keep an counter of elements when repeated to be seen, and keep a variable for setting majority element, as soon as we see the same
 # element , we increase its count, and as soon as we see some another element, we decrease the count till 0, when we change the majority element now to # # be at the current element as a new candidate and start the counter again , finally at the end, we will be having our majority element stored.
+# -------------------------------------------------------------------------------------------------------------------
+# NOTE : One important thing is that BOYER - MOORE VOTING ALGORITHM doesn't tell if the majority element exists, it gives the answer only
+# if the majority element exists.
+# So, we need to do a second linear time pass to count and check just for the candidates and their count if greater than required N//2, N//3..
+# This will allow us to get exactly how many majority elements are present or there are none of them existent.
+# --------------------------------------------------------------------------------------------------------------------
+
 def find_majority(arr):
     array_size = len(arr)
     count = 0
     M_element = None
     for i in range(array_size):
-        if count == 0: 
+        if count == 0:
             M_element = arr[i]
         if arr[i] == M_element:
             count += 1
         else:
             count -= 1
     return M_element
+
+# Majority element occuring |N/3| times :
+# --------------------------------------------------------------------------------------------------------------------
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        array_size = len(nums)
+        count1, count2, M_element1, M_element2 = 0, 0, None, None
+        for i in range(array_size):
+            if nums[i] == M_element1:
+                count1 += 1
+            elif nums[i] == M_element2:
+                count2 += 1
+            elif count1 == 0:
+                M_element1 = nums[i]
+                count1 += 1
+            elif count2 == 0:
+                M_element2 = nums[i]
+                count2 += 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        result = []
+        for i in [M_element1, M_element2]:
+            if nums.count(i) > len(nums) // 3:
+                result.append(i)
+        return result
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ALTERNATE METHODS FOR N/2 BUT ARE APPLICABLE FOR OTHERS ALSO WITH SLIGHT MODIFICATIONS..... (FOR SHOWING DIFFERENT METHODS PURPOSES)
 # METHOD 3 STARTS
 # sorting the array , then middle element will always be the majority element
 # time : 0(nlgn), space : 0(1)
