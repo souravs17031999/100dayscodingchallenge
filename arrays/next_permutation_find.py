8e2c2efc11ebef998698083dfb64e914c55ec191
Sourav kumar <souravs_1999@rediffmail.com>
2020-08-22 18:39:28 +0530

    update important comments

--- arrays/next_permutation_find.py
+++ arrays/next_permutation_find.py
@@ -8,9 +8,18 @@
 # Logic is to find first element pointer "i" at which index, (right->left) that element will be smaller than the just after the element.
 # because we have assumed array to be descending to it the last , so we are tending it towards it.
 # So, we need to know where this propert voilates, and then "i" points to that first element.
+# Like in some permutation : [1, 3, 5, 4, 2] => break point is 3 (index=1).
+
 # Then, we find first closer greater element (right->left), pointed at by "j".
-# We swap both of those to produce next permutation.
-# Now, we also sort the array after the pointer "i" till last element, in order to make it lexigraphically (sorted order).
+# Because we need lexpgraphically next greater element, so, we need the closest greater element that is 4, index(= 3).
+
+# We swap both of those to produce next permutation => [1, 4, 5, 3, 2]
+
+# Now, we also reverse the array after the pointer "i" till last element, in order to make it lexigraphically (sorted order) to get the lowest rank possible in order to further 
+# generate next permutations.
+
+# [1, 4, 2, 3, 5] => will give us the exact next permutation.
+
 # If at any point, "i" becomes -ve, that means the array is already sorted in descending order, hence we simply reverse it to
 # return the ascending array sorted order as there is no more next permutation possible for it.
 # TIME : 0(N), SPACE : 0(1).
