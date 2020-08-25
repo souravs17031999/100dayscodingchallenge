b13c8b803597200d368bdf2c08b5e3fa65bc9a20
sourav kumar <souravs_1999@rediffmail.com>
2020-08-25 13:08:31 +0530

    count all cycles without inner cycles in the graph

---
+++ graphs/count_all_cycles_graph.py
@@ -0,0 +1,3 @@
+# Given an undirected graph consisting of N vertices numbered [0, N-1] and E edges, the task is to count the number of cycles such that any
+# subset of vertices of a cycle does not form another cycle.
+# Since V vertices require V edges to form 1 cycle, these the number of required cycles can be expressed using the formula:  (Edges - Vertices) + 1
