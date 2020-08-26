1279ce27d9cdd924b8a331bd373d82740aa3c3bf
sourav kumar <souravs_1999@rediffmail.com>
2020-08-26 16:46:06 +0530

    count degree of cyclic graphs

---
+++ graphs/count_degree_cycle_graphs.py
@@ -0,0 +1,11 @@
+# Given the number of vertices in a Cycle Graph. The task is to find the Degree and the number of Edges of the cycle graph.
+# Degree: Degree of any vertex is defined as the number of edge Incident on it.
+# Cycle Graph: In graph theory, a graph that consists of single cycle is called a cycle graph or circular graph.
+# The cycle graph with n vertices is called Cn.
+# It is a Connected Graph.
+# A Cycle Graph or Circular Graph is a graph that consists of a single cycle.
+# In a Cycle Graph number of vertices is equal to number of edges.
+# A Cycle Graph is 2-edge colorable or 2-vertex colorable, if and only if it has an even number of vertices.
+# A Cycle Graph is 3-edge colorable or 3-edge colorable, if and only if it has an odd number of vertices.
+# In a Cycle Graph, Degree of each vertx in a graph is two.
+# The degree of a Cycle graph is 2 times the number of vertices. As each edge is counted twice.
