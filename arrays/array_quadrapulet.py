d6d40cd8eda9aefc29774bafd8fb3f7165809687
sourav kumar <souravs_1999@rediffmail.com>
2020-09-10 18:43:27 +0530

    more efficient method for 4target sum problem

--- arrays/array_quadrapulet.py
+++ arrays/array_quadrapulet.py
@@ -1,5 +1,6 @@
 # Program to first find all the quadraples, which sum up to some specific target sum, and then extend this by including only the unique quadraples.
 
+# Naive solution will be to generate all quadraples in 0(N^4).
 # below program is complete in the sense that it is already extended for only unique quadraples
 # logic is to set two pointers fixed for every iteration of two loops , and then use one more inner loop for solving two pair sum problem where the
 # target will be the remaining sum after subtraction the other numbers from the Original target.
@@ -49,8 +50,35 @@ def find_array_quadra(arr, s):
                 else:
                     end -= 1
     return result
+
+# -----------------------------------------------------------------------------------------------------------
+# Now, we will improve more to 0(N^2) and space 0(N^2) using hashing.
+# Idea is to store all pairs of sums into a hashmap containing keys as sums and values as tuple of indices.
+# Then, again generate all pairs from array and check for X - (pair sum current) if it is in the hashmap, if it is just check if all are now
+# distinct indices, then only that pair can be correct quadraples for the given sum otherwise we will generating wrong duplicates quadraples.
+
+def compute_quadraples(arr, n, x):
+
+    map = {}
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            map[arr[i] + arr[j]] = (i, j)
+
+    for i in range(0, n):
+        for j in range(i + 1, n):
+
+            if (x - (arr[i] + arr[j])) in map:
+                p = map[x - (arr[i] + arr[j])]
+                if p[0] != i and p[0] != j and p[1] != i and p[1] != j:
+                    print(arr[i], arr[j], arr[p[0]], arr[p[1]])
+                    return
+
+    return -1                
+
 if __name__ == '__main__':
-    assert find_array_quadra([2, 7, 4, 0, 9, 5, 1, 3],  20) == [[0, 4, 7, 9], [1, 3, 7, 9], [2, 4, 5, 9]]
-    assert find_array_quadra([1, 0, -1, 0, -2, 2], 0) == [[-2, -1, 1, 2], [-2,  0, 0, 2], [-1,  0, 0, 1]]
-    assert find_array_quadra([0, 0, 0, 0], 0) == [[0, 0, 0, 0]]
-    assert find_array_quadra([-3,-2,-1,0,0,1,2,3], 0) == [[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    arr = [10, 20, 30, 40, 1, 2]
+    compute_quadraples(arr, len(arr), 91)
+    # assert find_array_quadra([2, 7, 4, 0, 9, 5, 1, 3],  20) == [[0, 4, 7, 9], [1, 3, 7, 9], [2, 4, 5, 9]]
+    # assert find_array_quadra([1, 0, -1, 0, -2, 2], 0) == [[-2, -1, 1, 2], [-2,  0, 0, 2], [-1,  0, 0, 1]]
+    # assert find_array_quadra([0, 0, 0, 0], 0) == [[0, 0, 0, 0]]
+    # assert find_array_quadra([-3,-2,-1,0,0,1,2,3], 0) == [[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
