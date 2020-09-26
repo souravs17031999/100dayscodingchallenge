0a72334bad8fee71e30068bb91a20afc03a8b0aa
Sourav Kumar <souravs_1999@rediffmail.com>
2020-09-26 16:30:36 +0530

    update modifications for including duplicates to the array

--- arrays/search_rotated_sorted_array.py
+++ arrays/search_rotated_sorted_array.py
@@ -66,6 +66,51 @@ class Solution:
         return -1
 
 
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+# MOdification for including duplicates :
+
+class Solution:
+    def is_binary_search_helpful(self, nums, start, element):
+        return nums[start] != element
+    
+    def exists_in_first(self, nums, start, element):
+        return nums[start] <= element
+    
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return 1
+            
+            if not self.is_binary_search_helpful(nums, low, nums[mid]):
+                low += 1
+                continue
+            
+            pivot = self.exists_in_first(nums, low, nums[mid])
+            target_val = self.exists_in_first(nums, low, target)
+            
+            if (pivot) ^ (target_val):
+                
+                if pivot:
+                    low = mid + 1
+                
+                else:
+                    high = mid - 1
+            
+            else:
+                if nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        
+        return 0
+            
+            
+            
+
+
 # driver function
 if __name__ == '__main__':
     arr1 = [3, 6, 8, 9, 12, 14, 18, 21]
