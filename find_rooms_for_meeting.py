7591f3b697e13cbdf022607a7c3dbcac21454889
Sourav Kumar <souravs_1999@rediffmail.com>
2020-09-29 22:18:19 +0530

    add code for computing if meeting rooms can be accommodated in the given calendar days

--- find_rooms_for_meeting.py
+++ find_rooms_for_meeting.py
@@ -1,3 +1,4 @@
+# 1. Program for computing required rooms for conducting meetings such that no two meetings overlap over each other and also each room can conduct only one meeting at a time.
 
 # meeting time intervals = [[0, 30],[5, 10],[15, 20]]
 
@@ -25,6 +26,21 @@ def compute_conf_room(arr):
             end_i += 1
     
     return rooms
+    
+# # 2. Program for computing if given number of queries can be accomodated in the given calendar days and given number of rooms.
+# Input: calendar = [[1, 2], [4, 5], [8, 10]], rooms = 1, queries = [[2, 3], [3, 4]]
+# Output: [true, true]
+
+calendar = [[1, 2], [4, 5], [8, 10]]
+res = [None] * len(calendar)
+for i in range(len(calendar)):
+    calendar.append(queries[i])
+    if compute_conf_room(arr) <= rooms:
+        res[i] = True
+    calendar.remove(len(calendar) - 1)
+
+ return res
+    
     
 if __name__ == '__main__':
     
