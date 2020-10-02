bb40f31ae38518ee4cb95f4c18401634b585db15
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-02 16:37:21 +0530

    update comments for better clarity

--- graphs/floyd_warshall_algo.py
+++ graphs/floyd_warshall_algo.py
@@ -21,12 +21,25 @@
 # but after applying the algorithm , mat[3][1] = 4, because there is a path from 3 to 1 via 0 (considering weights given)
 # --------------------------------------------------------------------------------------
 # So, the logic of the algo is to check everytime for each of the vertices, if there is any shortest path possible via any other path
-# and this is done for every possible pair of path, and so it takes 0(N^3).
+# and this is done for every possible pair of path, and so it takes 0(V^3).
 # -----------------------------------------------------------------------------------------
-# This is also useful for detecting negative weight cycle, if the values at the diagonal of the matrix (originally 0), are all
+# This is also useful for detecting negative weight cycle, if the values at the diagonal of the matrix (originally 0) meaning distance from itself to itself, are all
 # negative numbers, then there is a negative weight cycle in the graph.
 # -----------------------------------------------------------------------------------------
 # DP ON GRAPHS.
+#
+# {0} ----- {3}                   [0, 3, inf, 5]
+# ||    /     |                   [2, 0, inf, 8]
+# || /        |                   [inf, 1, 0, inf] 
+# {1} ------{2}                   [inf, inf, 2, 0]
+#
+# Now, here four nodes are present, so total 4 times we will fill this matrix and compute distances.
+# First time, we will include 0, so now all the distances from 0 will be same, but now other distances will computed again by including 0 in its path :
+# like earlier, d[1][2] = min(d[1][2], d[1][0] + d[0][2]) => inf (no changed)
+# simi., d[1][3] = min(d[1][3], d[1][0] + d[0][3]) => 7 (changed from 8)
+# sim. other values will be filled.
+# Then, we will include 1, then fill all other values, sim. for 2, and 3 nodes.
+# ------------------------------------------------------------------------------------------------------------------------------
 
 import sys
 class Graph:
