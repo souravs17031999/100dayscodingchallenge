9466283596c84f387de81db14ddb1607f00caa49
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-06 12:36:08 +0530

    update function for more better clarity and conciseness

--- strings/custom_split_string.py
+++ strings/custom_split_string.py
@@ -1,6 +1,7 @@
 # Program to implement custom split function.
 # It handles all the corner cases for all the delimiters like for ex. : ";;asbabs;;;;;fkb;;;" / ";;;;;;;;" / "geeks;geeks;sourav" etc.. for delim ";"
 # Modify the below code as per the needs 
+# Here, we assume if string is started using delim or ended using delim, then we ignore that and if we in any case we want that it can be simply added on top of it.
 # TIME : 0(N), N IS LENGTH OF STRING.
 
 def split(s, delim):
@@ -9,25 +10,21 @@ def split(s, delim):
     word = "" # temporary strings inside the complete text (denoted by s)
     ptr = 0 # pointer to the current char inside (s)
     
-    # going char by char for the entire string 
+
     while ptr < len(s):
         
-        # if it is not delim, then we need to append it to the running word string 
         if s[ptr] != delim:
             word = "".join((word, s[ptr]))
         else:   
-            # now it handles the case for multiple occurences of delim consecutive, and also if string starts with delim
-            while ptr < len(s) and s[ptr] == delim:
-                ptr += 1
-            ptr -= 1
-            # at this point, we have complextely formed the word separated from delim, simply append to list
-            split_text.append(word)
-            # we need to reset it back to construct new string (if any)
+            if word != "":
+                split_text.append(word)
+                
             word = ""
         
-        # for considering last string (word)
-        if ptr == len(s) - 1:
+        # handles corner case for last word formed but shouldn't be equal to delim
+        if ptr == len(s) - 1 and s[ptr] != delim:
             split_text.append(word)
+            
         ptr += 1
     
     return split_text
