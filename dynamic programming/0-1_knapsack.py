bb1ddc750acc5bc8fa81084075cea9ee0af18159
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-09 12:18:58 +0530

    UPDATE THE DETAILED EXPLANATION OF RECURSION TREE FOR DP

--- dynamic programming/0-1_knapsack.py
+++ dynamic programming/0-1_knapsack.py
@@ -18,6 +18,57 @@
 # ----------------------------------------------------------------------------------------
 # The state will be repr by dp[item][weight], denote maximum value of ‘j-weight’ considering all values from ‘1 to ith’
 # TIME : 0(N * W), SPACE : 0(N * W)
+# ----------------------------------------------------------------------------------------
+# Detailed recursion tree explanation : 
+# EX. value : [60, 100, 120]                                      
+#    weight : [10, 20, 30]                        
+#    W = 50
+#                      
+#                         50,3
+#                          /    \
+#                       20,2      50,2   
+#                      /   \      /   \
+#                    10,1  20,1 30,1     50,1  
+#                           /\      /\       /    \
+#                        10,0 20,0 20,0 30,0 40,0  50, 0
+#
+# We can see overlapping subproblems here.
+# -------------------------------------------------------------------------------------------
+# Simple recursion : 
+# TIME : 0(2^N), SPACE : 0(1)
+#
+def knapsack(W, wt, val, n):
+    if W == 0 or n == 0:
+        return 0
+    
+    if wt[n - 1] <= W:
+         
+        return max(val[n-1] + knapsack(W-wt[n-1], wt, val, n-1),  knapsack(W, wt, val, n-1))
+    else:
+        return knapsack(W, wt, val, n-1)
+
+
+# TOP DOWN MEMOIZED USING DYNAMIC PROGRAMMING APPROACH : 
+# TIME : 0(W * N), SPACE : 0(W * N)
+
+def knapsack(W, wt, val, n):
+    print(W, wt, val, n)
+    if W == 0 or n == 0:
+        return 0
+    
+    if (W, n) in cache:
+        return cache[(W, n)]
+    
+    if wt[n - 1] <= W:
+        cache[(W, n)] = max(val[n-1] + knapsack(W-wt[n-1], wt, val, n-1),  knapsack(W, wt, val, n-1))
+        return cache[(W, n)]
+    else:
+        cache[(W, n)] = knapsack(W, wt, val, n-1)
+        return cache[(W, n)]
+    
+
+# BOTTOM UP TABULATED DP : 
+# TIME : 0(W * N), SPACE : 0(W * N)
 
 def compute_max_value(val, wt, W, n):
 
