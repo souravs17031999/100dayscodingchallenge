8deb997cdbd13229285338fd8fa7de92c30bcf85
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-10 17:39:51 +0530

    update code for finding longest repeating subsequences

--- dynamic programming/longest_repeating_subsequence.py
+++ dynamic programming/longest_repeating_subsequence.py
@@ -16,3 +16,24 @@
 # But if they occur in single occurnece, like "E" and "C" in the above, hence that's way they donot become part of longest repeating subsequence.
 # Thus, simply pass same both strings in LCS(S1, S2) and just add one more condition along with if condition, "i != j", other things remain same.
 # ----------------------------------------------------------------------------------------------------------------------------------------------
+def findLongestRepeatingSubSeq( str): 
+ 
+    n = len(str) 
+ 
+    # Create and initialize DP table 
+    dp=[[0]*(n+1)]*(n+1)
+ 
+    # Fill dp table (similar to LCS loops) 
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            # If characters match and indexes are 
+            # not same 
+            if (str[i-1] == str[j-1] and i != j): 
+                dp[i][j] = 1 + dp[i-1][j-1]         
+                         
+            # If characters do not match 
+            else:
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]) 
+         
+     
+    return dp[n][n] 
