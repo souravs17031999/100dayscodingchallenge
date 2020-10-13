b3ba6142ff48d9bcdd3560ac2d43318e6de7b515
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-13 11:38:53 +0530

    update comments for better info

--- strings/substrings_subsequences.py
+++ strings/substrings_subsequences.py
@@ -1,6 +1,8 @@
 # program for generating substrings and subsequences for a given array
 
 # program for generating subarrays/ substrings
+# TIME : 0(N^2).
+
 def gen_subarray(arr, n):
     print('All generating subarrays/substrings : ')
     # iterate through the list by keeping one point at one side
@@ -12,7 +14,12 @@ def gen_subarray(arr, n):
 
 
 # program for generating subsequences
-# IDEA: this is based on the fact that we can generate 2**n - 1 subsequences like counting in binary terms 000..00 till 11...11 and this can be useful here like when we want to print the subsequence , then we can simply test the following condition by anding the counter with all possible sequences of bit shifts by 0, 2, 4, 8.... so that all of them can be generated
+# IDEA: this is based on the fact that we can generate 2**n - 1 subsequences like counting in binary terms 000..00 till 11...11 and this can be useful here like when we want to print the subsequence , 
+# then we can simply test the following condition by anding the counter with all possible sequences of bit shifts by 0, 2, 4, 8.... so that all of them can be generated
+# So, we go one by one for all binary strings like 0, 1, 2, 3, ... 2**n-1, and check for all bit possible (till length of given string), so that whichever bit is set, that index of 
+# of array is considered, so like in "010", only 2nd index element will be considered in one subset, and in like "110", only first and second index element will be in one subset.
+# TIME : 0((2 ^ N) * (len(s))) ~ 0(2^N)
+
 def gen_subseq(arr, n):
     print('All generating subsequence : ')
     # iterate for all possible consequences
@@ -23,6 +30,8 @@ def gen_subseq(arr, n):
             if counter & (1<<j):
                 print(arr[j], end = " ")
         print()
+       
+    
 
 # main function
 if __name__ == '__main__':
