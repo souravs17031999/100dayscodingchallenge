23eb4e9c4cc6d76a77d7411089e8190033a73e30
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-13 21:41:35 +0530

    update comments for recursion tree viz and time complexity

--- arrays/combinations_generate.py
+++ arrays/combinations_generate.py
@@ -1,3 +1,22 @@
+# Program for generating and printing all combinations of size "r".
+# Approach will be to fix one one element at its position, and then either it will choose the current element or not choose the current element
+# same as the one for subset/subsequence generation.
+# Following is recursion tree visualized  :
+#                    (0,0)
+#                  /         \
+#             (1,1)          (0,1)
+#           /    \              /   \
+#       (2,2)    (1,2)         (1,1) (0,2)
+#       /\          /      \           
+#   (3,3) (2,3)   (2,3)     (1,3)  
+#          /\         /\          / \        /  (1,5)
+#        (3,4) (2,4) (3,4) (2,4) (2,4) (1,4) \  (2,5)
+#               /\          / \      / \   
+#         (3,5) (2,5) (3,5) (2,5) (3,5) (2,5)
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+# TIME : 0(2 ^ n) tighter upper bound whereas upper bound can be 0(nCr).
+
 arr = [1, 2, 3, 4, 5]
 
 def combinations(arr, n, r, index, temp, i):
