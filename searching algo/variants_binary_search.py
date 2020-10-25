c8e91ada5527fad5fe98084634162b4690cfcf28
sourav kumar <souravs_1999@rediffmail.com>
2020-10-25 11:03:17 +0530

    add more methods for modified binary search

--- searching algo/variants_binary_search.py
+++ searching algo/variants_binary_search.py
@@ -34,3 +34,36 @@ def first_occurence(self, arr, key):
 
             return pos
   
+def first_least_greater(arr, key):
+    
+    pos = -1
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        
+        if key < arr[mid]:
+            pos = mid
+            end = mid - 1
+        elif key > arr[mid]:
+            start = mid + 1
+        else:
+            start = mid + 1
+    
+    return pos
+
+def last_greatest_lesser(arr, key):
+    
+    pos = -1
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        
+        if key < arr[mid]:
+            end = mid - 1
+        elif key > arr[mid]:
+            pos = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    return pos
