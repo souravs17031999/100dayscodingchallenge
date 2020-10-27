81837debf7cbf557ff9ef983ba0b5867d4f733aa
Sourav Kumar <souravs_1999@rediffmail.com>
2020-10-27 18:29:08 +0530

    update simulation and more comments for intuition

--- arrays/maximum_every_subarray_k_window_sliding.py
+++ arrays/maximum_every_subarray_k_window_sliding.py
@@ -14,17 +14,7 @@
 # Output: 10 10 10 15 15 90 90
 # -------------------------------------------------------------------------------------------------------------------------------------
 # Naive solution would take 0(N^2) => using two loops, one for outer loop and one inner loop for total array and k size block traversal respectively.
-
-# using deque DS from collections to build linear time solution as following the logic below
-# basically, idea is to keep a track of all useful elements for a window which keeps sliding across the array one by one, the useful element is one which
-# is greater than all right elements in the current window.
-# so, basically for every window we need maximum in 0(1) so that we can compare it with incoming new element and then either keep it or remove it,
-# and simulatenously also sliding the window, so the best option is to use Deque so that we will be able to slide the window by popping the elements from
-# left side which is not in current window, and also keep track of maximum at the front by comparing it with each incoming element and also one important
-# thing is that, we need to keep minimum as it at the right side, as [3...2..0...-1], so if cur_max is 3, then we also need to have 2 because maybe in
-# next window, we can have 2 as maximum, threby it will maintain all indices of elements in decreasing order.
-# time : 0(N), space : 0(K)
-
+# ------------------------------------------------------------------------------------------------------------------------------------------------
 # We can also try window sliding techniuqe and simulate it using two pointers both pointing at start and end of k-sized window.
 # Then, we move one pointer one by one k times and update the max, till it becomes equal to second pointer, there we got max for that subarray
 # now, we have find the maximum of subarrays of that particular k sized window, then we go for increment second pointer, and then first pointer by 1 from
@@ -32,6 +22,40 @@
 # In this way, we go through all the subsarrays , this method is actually same as first one, just the way of understanding is different and that's why
 # coding is slightly different but overall asymtodic bounds are same => 0(N * K) which is not better but in space it's 0(1).
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------
+# using deque DS from collections to build linear time solution as following the logic below
+# basically, idea is to keep a track of all useful elements for a window which keeps sliding across the array one by one, the useful element is one which
+# is greater than all right elements in the current window.
+# so, basically for every window we need maximum in 0(1) so that we can compare it with incoming new element and then either keep it or remove it,
+# and simulatenously also sliding the window, so the best option is to use Deque so that we will be able to slide the window by popping the elements from
+# left side which is not in current window, and also keep track of maximum at the front by comparing it with each incoming element and also one important
+# thing is that, we need to keep minimum as it at the right side, as [3...2..0...-1], so if cur_max is 3, then we also need to have 2 because maybe in
+# next window, we can have 2 as maximum, threby it will maintain all elements (indices) of elements in decreasing order.
+# time : 0(N), space : 0(K)
+#
+# |1, 2, 3|, 1, 4, 5, 2, 3, 6 => deque : [3]
+#         ^
+# 1 |2 3 1| 4 5 2 3 6 => deque : [3, 1]
+#        ^                       
+# 1 2 |3 1 4| 5 2 3 6 => deque : [4]
+#          ^                     
+# 1 2 3 |1 4 5| 2 3 6 => deque : [5]
+#            ^
+# 1 2 3 1 |4 5 2| 3 6 => deque : [5, 2]
+#              ^
+# 1 2 3 1 4 |5 2 3| 6 => deque : [5, 3]
+#                ^ 
+# 1 2 3 1 4 5 |2 3 6| => deque : [6]
+#                  ^ 
+# The deque is empty now , final output as seen from above simulation : 3, 3, 4, 5, 5, 5, 6
+#
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# We can also apply heap based sliding window approach, where max-heap can be used to obtain max from k sized heap in 0(1), but the problem is in getting random access 
+# when we move out of theh window, we will not be directly able to delete the non useful elements (whcih are not part of curr window) as we need their indices to delete 
+# from heap, and so we find the index in the heap, and replace it with the next obtained element in the current wndow.
+# Max-heaify to restore heap, and get the max from root, and then increment pointers i and j poitning for window control.
+# Asymtodically, this will also take 0(N * K) so, deque based in 0(N) is probably good enough for this problem.
+# There are more methods like using self-BST for solving this problem in 0(N * log(K))
+# --------------------------------------------------------------------------------------------------------------------------------------------
 
 from collections import deque
 def maximum_subarray(arr, k):
