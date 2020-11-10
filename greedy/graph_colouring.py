504b2e1f1f7676065db2404aefadcae5554e32ea
Sourav Kumar <souravs_1999@rediffmail.com>
2020-11-10 22:02:35 +0530

    update comments + intuition

--- greedy/graph_colouring.py
+++ greedy/graph_colouring.py
@@ -1,6 +1,11 @@
 # Problem : Given a graph, we need to color all the vertices such that no two adjacent vertices
 # should be coloured same.
 # ------------------------------------------------------------------------------------
+# NOTE : Firstly, we need to convince ourselves that given a graph whose max degree of any node = "D", can be coloured properly with ATMOST = "D+1" COLORS.
+# Brute force way of colouring would be to check for all possible orderings of all colors, like say we have graph with maximum degree of D = 3, and then it would be coloured
+# with atmost 4 colors, now if we start checking for all combinations : 
+# Total : 4^12 , in general : 0(D^N) combinations and that would be exponential.
+# ------------------------------------------------------------------------------------
 # Colouring the graph is NP-HArd problem where there is no efficient optimal solution
 # but there is a quick algorithm that halts and gives a good approximate in number of situations.
 # So, we apply here greedy approach to colour the first vertex and second which ever is avaiable
@@ -13,7 +18,10 @@
 # and similarly for the remaining vertices.
 # The greedy part is that we are assinging whichever color is presently avaiable and don't
 # think about the future or reconsider our past decision.
+# TIME : 0(D * N), D IS MAX DEGREE, N IS TOTAL NODES.
 # -------------------------------------------------------------------------------------
+# Can we do better ?
+# We can reduce this to linear time in 0(E + V).
 
 from collections import defaultdict as dd
 class Graph:
