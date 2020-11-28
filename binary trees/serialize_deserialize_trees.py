32d70ab002110c83e2ced63a51f0c332bb302023
sourav kumar <souravs_1999@rediffmail.com>
2020-11-28 19:32:21 +0530

    UPDATE EXAMPLES + COMMENTS

--- binary trees/serialize_deserialize_trees.py
+++ binary trees/serialize_deserialize_trees.py
@@ -1,5 +1,29 @@
 # Program for serialization and deserialization of binary trees.
 # -------------------------------------------------------------------------------------
+# Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or
+# memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+# You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+#            1
+#           / \
+#          2   3
+#             / \
+#            4   5
+# -------------------------------------------------------------------------------------------
+# Input: root = [1,2,3,null,null,4,5]
+# Output: [1,2,3,null,null,4,5]
+# Example 2:
+#
+# Input: root = []
+# Output: []
+# Example 3:
+#
+# Input: root = [1]
+# Output: [1]
+# Example 4:
+#
+# Input: root = [1,2]
+# Output: [1,2]
+# --------------------------------------------------------------------------------------
 # LOGIC FOR serialization :
 # Now, important key is to think in terms of traversals -
 # Preorder is kind of good to go !
